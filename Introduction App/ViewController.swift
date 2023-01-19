b1d578a1036a6cbf7a0d7dd73394cb3d86d6c508
liltaqvi <alihussaintaqvi14@yahoo.com>
2023-01-19 16:29:26 -0600

    Update ViewController.swift

--- Introduction App/ViewController.swift
+++ Introduction App/ViewController.swift
@@ -9,11 +9,48 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    
+    @IBOutlet weak var morePetsStepper: UIStepper!
+    
+    @IBAction func updatePetNumber(_ sender: UIStepper) {
+        numberOfPetsLabel.text = "\(Int(sender.value))"
+    }
+    
+    @IBAction func presentApp(_ sender: UIButton) {
+        // Lets us choose the title we have selected from the segmented control
+        // In our example that is whether it is first, second, third, or fourth
+        let year = yearSegmentedControl.titleForSegment(at: yearSegmentedControl.selectedSegmentIndex)
+        
+        // Creating a constant of type string that holds an introduction. The introduction recieves the values from the outlet connections
+        let introduction = "My name is \(firstNameTextField.text!) \(lastNameTextField.text!) and I attend the \(schoolNameTextField.text!). I am currently in my \(year!) year and I own \(numberOfPetsLabel.text!) dogs. It is \(morePetsSwitch.isOn) that I want more pets."
+        
+        // Creates the alert where we pass in our message, which out introduction
+        let alertController = UIAlertController(title: "My Introduction", message: introduction, preferredStyle: .alert)
+        
+        // A way to dismiss the box once it pops up
+        let action = UIAlertAction(title: "Nice to meet you!", style: .default)
+        
+        // Passing this action to the alert controller so it can be dismissed
+        alertController.addAction(action)
+        
+        present(alertController, animated: true, completion: nil)
+    }
+    
+    @IBOutlet weak var userInfo: UIButton!
+    
+    @IBOutlet weak var morePetsSwitch: UISwitch!
+    
+    @IBOutlet weak var firstNameTextField: UITextField!
+    @IBOutlet weak var lastNameTextField: UITextField!
+    @IBOutlet weak var schoolNameTextField: UITextField!
+    
+    
+    @IBOutlet weak var yearSegmentedControl: UISegmentedControl!
+
+    @IBOutlet weak var numberOfPetsLabel: UILabel!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
-
-
 }
 
